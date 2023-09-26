c12cdbee806317a8f2d768359f444ecfd02fd80c
imriiama <146062070+imriiama@users.noreply.github.com>
2023-09-26 15:55:53 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+var a = 3; // mettre let au mlieu de var
+while(a < 9){
+    a++; //; pas nécessaire 
+    console.log(a) 
+    if (a == 8){
+        break; //; pas nécessaire 
+    } else if (a == 7) {
+        continue; //; pas nécessaire 
+    }
+    // console.log(a) au mauvais endroit
+}
+
+// correction : 
