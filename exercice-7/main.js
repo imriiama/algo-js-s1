ba0cf290975f027aaa99c186dfa9724886c09c37
imriiama <146062070+imriiama@users.noreply.github.com>
2023-09-27 11:25:42 +0200

    sauv

---
+++ exercice-7/main.js
@@ -0,0 +1,9 @@
+let ProfilPerso = {
+    FirstName : "Miss",
+    LastName : "Fortune",
+    Age : 15,
+}
+
+let sentence = ProfilPerso.FirstName + " " + ProfilPerso.LastName + " a " + ProfilPerso.Age + " ans" + " et est à l'école."
+
+console.log(sentence)
