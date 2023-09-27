eeef2417d3a6d0f86ac11b016ab3072e50c89ee5
imriiama <146062070+imriiama@users.noreply.github.com>
2023-09-27 12:56:26 +0200

    sauv

--- exercice-7/main.js
+++ exercice-7/main.js
@@ -1,3 +1,6 @@
+/* Exo : créer un profil pour un personnage, le nom de la variable qu’on veut, ça doit être un objet, 
+au moins 3 clés : prenom, nom, age, et afficher dans la console une phrase avec console.log (prenom et nom a age et est à l’IIM.*/
+    
 let ProfilPerso = {
     FirstName : "Miss",
     LastName : "Fortune",
---
+++ exercice-8/main.js
@@ -0,0 +1,68 @@
+class pokemeon {
+    constructor(name, attack, defense, hp, luck){
+        this.name = poke1, poke2
+        
+
+        attackPokemon(poke1, poke2) {
+            poke1.hp -= poke2
+        }
+    }
+    name;
+    attack;
+    defense;
+    hp;
+    luck;
+
+    luck = Math.random(0-1) 
+    function isLucky (Pokemon_luck, luck) {
+    return (Pokemon_luck < luck);
+    }
+}
+
+/*let Poke1 = new Pokemeon("Poke1", 20, 10, 5, 0.6);
+let Poke2 = new Pokemeon("Poke2", 15, 5, 2, 0.4);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+while (Poke1_hp > 0 || Poke2_hp > 0){
+    let degat1 = Poke1_attack - Poke2_defense
+    Poke2_hp -= degat1
+    console.log("hp : " + Poke2_hp, "degats : " + degat1)
+    if (Poke2_hp <= 0){
+        break
+        console.log("Poke2 est mort.")
+    }
+    let degat2 = Poke2_attack - Poke1_defense
+    Poke1_hp -= degat2
+    console.log("hp : " + Poke1_hp, "degats : " + degat2)
+    if (Poke1_hp <= 0){
+        break
+        console.log("Poke1 est mort.")
+    }
+}
+
+/*luck = Math.random(0-1) 
+function isLucky (Pokemon_luck, luck) {
+    return (Pokemon_luck < luck);
+}*/
