f003de29493174dd16a8fb7a88e655cd11153239
imriiama <146062070+imriiama@users.noreply.github.com>
2023-10-01 17:33:39 +0200

    sauv

---
+++ exo2-le tueur/main.js
@@ -0,0 +1,20 @@
+class tueur {
+    constructor(name, pv){
+        this.name = name
+        this.pv = pv
+    }
+}
+
+let jason = new tueur("Jason", 100)
+
+let caract = ["nerd", "sportif", "blonde"]
+let name = ["Harry", "Hermione", "Weasley" ]
+
+let random_name = Math.floor(Math.random() * caract.length)
+
+let random_carac = Math.floor(Math.random() * caract.length)
+
+//console.log("Nom aleatoire : " + caract[random])
+
+
+
